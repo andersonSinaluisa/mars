049a5495255c8fb1fb215bd00a80521c19378e84
Jordan Dev <rjaramilloby@gmail.com>
2020-09-26 21:26:37 -0500

    Revert "Commit inicial del servidor, login y registro listo"
    This reverts commit e267f37507ade17abe47bdf21c4fa83d2c4b9dbb.

--- servidor/src/helpers/validator.js
+++
@@ -1,9 +0,0 @@
-const { validationResult } = require("express-validator");
-
-const validationFields = (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty())
-    return res.status(401).json({ msg: errors.errors[0].msg });
-};
-
-module.exports = validationFields;
