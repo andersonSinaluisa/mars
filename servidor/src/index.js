ac609a35b1ed83ce69ab474c14a8b436a21416e7
Jordan Dev <rjaramilloby@gmail.com>
2020-09-29 22:23:02 -0500

    Socket implementado falta configurarlo

--- servidor/src/index.js
+++ servidor/src/index.js
@@ -3,15 +3,21 @@ const morgan = require("morgan");
 const conectarDB = require("./database");
 const cors = require("cors");
 require("dotenv").config();
+const io = require("socket.io");
 const app = express();
+const http = require("http")(app);
 conectarDB();
 app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
 
 app.use(morgan("dev"));
 app.use(express.json({ extended: true }));
 
 app.use("/api/auth", require("./routes/auth.routes"));
 
-app.listen(process.env.PORT || 3400, process.env.ip, function () {
+http.listen(process.env.PORT || 3400, process.env.ip, function () {
   console.log("Server is running!");
 });
